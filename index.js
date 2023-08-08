a2dc9625aee81b9e4ab9ce3ace00d40f7efb6785
kumar_sushil <romanreins488@gmail.com>
2023-08-08 21:25:44 +0530

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const connect = require('./Db/Connection');
 const cors = require('cors');
 const UserRoute = require('./Routes/UserRoutes');
 const blogRoute = require('./Routes/BlogRoutes');
-const path = require('path')
 const dotenv = require('dotenv')
 dotenv.config();
 
@@ -19,16 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(UserRoute);
 app.use(blogRoute);
-app.use(express.static(path.join(__dirname,'./client/dist')));
 
-
-// app.get('/get', (req,res)=>{
-//     res.send({message:'success'})
-// })
-
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./client/dist/index.html'))
-})
 
 app.listen(port, ()=>{
     connect();
